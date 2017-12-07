5a6c64bd9fd44824a13934c47a04be0cdf24cd59
Joseph Cobb <jac111@duke.edu>
2017-12-07 17:34:24 -0500

    Suyash to the rescue?

--- PredictMelanomaServer.py
+++ PredictMelanomaServer.py
@@ -78,7 +78,10 @@ def melanoma_prediction():
 
 
     imgdata = base64.b64decode(b64img_string)
-    img = mpimg.imread(imgdata)
+    with open("temp.jpg", "wb") as image_out:
+        image_out.write(imgdata)
+    print image_out
+    img = mpimg.imread("temp.jpg")
 
 
 
