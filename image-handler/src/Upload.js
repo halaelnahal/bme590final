05df369ff93f741e14b406561c6f3c38676411f1
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-12-07 16:30:16 -0500

    added axios post method to try to upload to server

--- image-handler/src/Upload.js
+++ image-handler/src/Upload.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import DropzoneComponent from 'react-dropzone-component';
 import { UploadField } from '@navjobs/upload';
 import axios from 'axios'
+
 class Upload extends Component {
 	constructor() {
 		super();
@@ -11,13 +12,20 @@ class Upload extends Component {
 		}
 	}
 
+    parsePrediction = (json_output) => {
+        json_output["malignant"];
+    }
+
+
 	onUpload = (files) => {
 		const reader = new FileReader()
 		const file = files[0]
+		var prediction_result
 		reader.readAsDataURL(file);
 		reader.onloadend = () => {
 			console.log(reader.result);
 			this.setState({currentImageString: reader.result});
+			prediction_result = axios.post('http://vcm-1855.vm.duke.edu:8000/melanoma/prediction',this.state).then(this.parsePrediction);
 		}
 	}
 
