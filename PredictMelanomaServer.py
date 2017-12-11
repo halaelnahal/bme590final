25c6a5b4f08bc0a3741a7af6b738eb26cd41f36d
Joseph Cobb <jac111@duke.edu>
2017-12-10 23:56:35 -0500

    implemented error handling. Closes #2. Closes #6. Closes #18. Closes #19.

--- PredictMelanomaServer.py
+++ PredictMelanomaServer.py
@@ -37,6 +37,13 @@ def send_error(message, code):
     return jsonify(err), code
 
 
+def validate(img):
+    if img.shape[2] == 3:
+        return True
+    else:
+        return False
+
+
 @app.route("/server")
 def test_server():
     output = {"Results": "Server is up and running!"}
@@ -46,18 +53,43 @@ def test_server():
 @app.route("/decode/base_64", methods=['POST'])
 def test_base_64():
     from base64 import b64decode
-    b64text = request.json["sample"]
-    text = b64decode(b64text)
+
+    try:
+        b64text = request.json["sample"]
+        text = b64decode(b64text)
+
+    except KeyError:
+        return send_error("Invalid input. Input JSON dictionary. key: sample"
+                          " value: base 64 string", 400)
+
+    except TypeError:
+        return send_error("Cannot decode base 64 string", 400)
+
+    except UnicodeDecodeError:
+        return send_error("Problem decoding input", 400)
+
     output = {"Results": text}
+
     return jsonify(output)
 
 
 @app.route("/sample_image", methods=['POST'])
 def test_sample():
-    if request.json["currentImageString"] != '':
-        output = {"Results": "Received!"}
-    else:
-        output = {"Results": "Not received."}
+    try:
+        if request.json["currentImageString"] != '':
+            output = {"Results": "Received!"}
+        else:
+            output = {"Results": "Not received."}
+
+    except KeyError:
+        return send_error("Invalid input. Input dictionary. key:"
+                          " currentImageString value: image encoded as base 64"
+                          " string", 400)
+
+    except TypeError:
+        return send_error("Invalid input. Input dictionary. key:"
+                          " currentImageString value: image encoded as base 64"
+                          " string", 400)
 
     return jsonify(output)
 
@@ -68,15 +100,38 @@ def melanoma_prediction():
     from base64 import decodestring
     from matplotlib.image import imread
 
-    raw_string = request.json["currentImageString"]
-    start_index = raw_string.find(",") + 1
-    b64img_string = raw_string[start_index:]
-    imgdata = decodestring(b64img_string)
-    filename = "temp.jpg"
-    with open(filename, "wb") as image_out:
-        image_out.write(imgdata)
-    img = imread(filename)
-    (labels, predictions) = get_prediction(img)
+    try:
+        raw_string = request.json["currentImageString"]
+        start_index = raw_string.find(",") + 1
+        b64img_string = raw_string[start_index:]
+        imgdata = decodestring(b64img_string)
+        filename = "temp.jpg"
+        with open(filename, "wb") as image_out:
+            image_out.write(imgdata)
+        img = imread("top.jpg")
+        if validate(img) is False:
+            raise AssertionError
+        (labels, predictions) = get_prediction(img)
+
+    except KeyError:
+        return send_error("Invalid input. Input dictionary. key: "
+                          "currentImageString value: image encoded as base 64 "
+                          "string", 400)
+
+    except UnicodeDecodeError:
+        return send_error("Problem decoding input", 400)
+
+    except IOError:
+        return send_error("Please input base 64 encoded image", 400)
+
+    except AssertionError:
+        return send_error("Wrong img size. Expected numpy.ndarray of shape "
+                          "(x, y, 3)", 500)
+
+    except ValueError:
+        return send_error("get_prediction interrupted by error", 500)
+
     results = {"type": str(type(img)), "shape": str(img.shape),
                "labels": str(labels), "predictions": str(predictions)}
+
     return jsonify(results)
